9a9f0725f21134bc46be561a0b24c76baa0d8343
ufoo68 <mitarou.yuutarou@gmail.com>
2021-12-24 02:09:00 +0900

    debug

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { ProfileCard } from './ui-components';
 import liff from '@line/liff';
 import './App.css';
 import { Profile } from './models';
-import Amplify, { DataStore } from 'aws-amplify';
-import awsconfig from "./aws-exports";
-
-Amplify.configure(awsconfig);
+import { DataStore } from 'aws-amplify';
 
 function App() {
   useEffect(() => {
@@ -17,6 +14,7 @@ function App() {
       const profile = await liff.getProfile();
       const profiles = await DataStore.query(Profile);
       console.log(profiles);
+      console.log(profile.userId);
       const { bio, background_image } = profiles.find((value) => value.liff_id === profile.userId);
       setProfile({
         avatorImageSrc: profile.pictureUrl,
@@ -36,7 +34,7 @@ function App() {
   });
   return (
     <div className="App">
-      <ProfileCard ProfileCard={{bio: profile.bio, background_image: profile.backgroundImageSrc}} overrides={{
+      <ProfileCard ProfileCard={{ bio: profile.bio, background_image: profile.backgroundImageSrc }} overrides={{
         "View.Image[1]": {
           alt: 'アバター',
           src: profile.avatorImageSrc,
